649e7de30bfcd037554ece21df552272fa251dc9
BoujiDev <dev2024loper@gmail.com>
2024-10-15 17:55:42 +0330

    setup func

--- snake.cpp
+++ snake.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<Windows.h>
 using namespace std;
 
+int score;
+int const  width = 20, height = 40;
+bool gameOver;
+
+struct point {
+	int x, y;
+};
+point player, fruit;
+
+
+
 void setup() {
+	player.x = width / 2;
+	player.y = height / 2;
 
+	fruit.x = rand() % width;
+	fruit.y = rand() % height;
 
+	gameOver = false;
+	score = 0;
 
 }
 
@@ -24,12 +45,19 @@ void logic() {
 
 }
 
-//
+
 
 int main() {
 
+	srand(time(NULL));
+	setup();
 
-
+	while (!gameOver) {
+		draw();
+		input();
+		logic();
+		sleep(50);
+	}
 
 
 }
